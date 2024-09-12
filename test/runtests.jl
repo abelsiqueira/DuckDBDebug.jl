3eb6d2b68e559e76799673675047e6451f4ac64b
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2024-09-12 18:50:45 +0200

    Do more work

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,22 @@
+using DuckDB
 using Test
 
-@test true
+open("data.csv", "w") do io
+  println(
+    io,
+    """x,y
+1,2
+2,3
+3,5
+4,8
+"""
+  )
+end
+
+con = DBInterface.connect(DuckDB.DB)
+data = DuckDB.execute(con, "SELECT * FROM read_csv('data.csv')")
+show(data) # "realize"
+x, y = data.tbl.x, data.tbl.y
+
+@test x == [1, 2, 3, 4]
+@test y == [2, 3, 5, 8]
